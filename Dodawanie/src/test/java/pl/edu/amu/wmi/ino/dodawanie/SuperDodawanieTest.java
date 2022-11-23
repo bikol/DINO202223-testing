24a7e843706087bd984e68fe2eca8d36c36bc683
Patryk Żywica <bikol@amu.edu.pl>
2022-11-23 17:39:27 +0100

    nowy test

---
+++ Dodawanie/src/test/java/pl/edu/amu/wmi/ino/dodawanie/SuperDodawanieTest.java
@@ -0,0 +1,35 @@
+package pl.edu.amu.wmi.ino.dodawanie;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author bikol
+ */
+public class SuperDodawanieTest {
+    
+    public SuperDodawanieTest() {
+    }
+
+    @Test
+    public void testDodaj1() {
+        System.out.println("dodaj");
+        String a = "1";
+        String b = "1";
+        String expResult = "2";
+        String result = SuperDodawanie.dodaj(a, b);
+        assertEquals(expResult, result);
+    }
+    
+    @Test
+    public void testDodaj2() {
+        System.out.println("dodaj");
+        String a = "0";
+        String b = "0";
+        String expResult = "0";
+        String result = SuperDodawanie.dodaj(a, b);
+        assertEquals(expResult, result);
+    }
+    
+}
