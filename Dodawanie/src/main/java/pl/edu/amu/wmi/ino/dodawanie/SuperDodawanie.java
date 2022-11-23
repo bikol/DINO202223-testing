fda8af9413f401ef4711cf326189477989729134
Patryk Żywica <bikol@amu.edu.pl>
2022-11-23 17:40:22 +0100

    nowy test

---
+++ Dodawanie/src/main/java/pl/edu/amu/wmi/ino/dodawanie/SuperDodawanie.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.edu.amu.wmi.ino.dodawanie;
+
+/**
+ *
+ * @author bikol
+ */
+public class SuperDodawanie {
+    
+    public static String dodaj(String a, String b){
+        return "";
+    }
+    
+}
