19e8058dd4ea64e5a4de8108e52e954d809f3fc9
Ulad <vlad.zholydev@gmail.com>
2022-11-27 22:51:36 +0100

    Implementation corrected for tests 1-5

--- Dodawanie/src/main/java/pl/edu/amu/wmi/ino/dodawanie/SuperDodawanie.java
+++ Dodawanie/src/main/java/pl/edu/amu/wmi/ino/dodawanie/SuperDodawanie.java
@@ -11,11 +11,43 @@ package pl.edu.amu.wmi.ino.dodawanie;
  */
 public class SuperDodawanie {
 
-    public static String dodaj(String a, String b) {
-        Integer x = Integer.parseInt(a);
-        Integer y = Integer.parseInt(b);
-        Integer ans = x + y;
-        return ans.toString();
+
+    public static boolean isFloat(String s) {
+        try {
+            Float.parseFloat(s);
+        } catch(NumberFormatException e) {
+            return false;
+        } catch(NullPointerException e) {
+            return false;
+        }
+        return true;
     }
-    
+
+    public static boolean isInteger(String s) {
+        try {
+            Integer.parseInt(s);
+        } catch(NumberFormatException e) {
+            return false;
+        } catch(NullPointerException e) {
+            return false;
+        }
+        return true;
+    }
+
+
+    public static String dodaj(String a, String b){
+        if(isInteger(a) && isInteger(b)){
+            Integer x = Integer.parseInt(a);
+            Integer y = Integer.parseInt(b);
+            Integer ans = x + y;
+            return ans.toString();
+        }
+        if(isFloat(a) && isFloat(b)){
+            Float x = Float.parseFloat(a);
+            Float y = Float.parseFloat(b);
+            Float ans = x + y;
+            return ans.toString();
+        }
+        return a + b;
+    }
 }
