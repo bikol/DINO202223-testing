114dd7db05e8d9e64dbed04770a49a00bfcd201a
s473573 <edupok@st.amu.edu.pl>
2022-11-30 03:21:12 +0100

    Implement adding fractions

--- Dodawanie/src/main/java/pl/edu/amu/wmi/ino/dodawanie/SuperDodawanie.java
+++ Dodawanie/src/main/java/pl/edu/amu/wmi/ino/dodawanie/SuperDodawanie.java
@@ -34,8 +34,30 @@ public class SuperDodawanie {
         return true;
     }
 
+    //private static
+    public static boolean isFrac(String s)
+    {
+        if (s.chars().filter(c -> c == '/').count() != 1)
+            return false;
+
+        String[] frac = s.split("/");
+
+        return isInteger(frac[0]) && isInteger(frac[1]);
+    }
+
+    public static float parseFrac(String s)
+    {
+        float den, num;
+
+        String[] frac = s.split("/");
+        num = Float.parseFloat(frac[0]);
+        den = Float.parseFloat(frac[1]);
+
+        return num/den;
+    }
 
     public static String dodaj(String a, String b){
+
         if(isInteger(a) && isInteger(b)){
             Integer x = Integer.parseInt(a);
             Integer y = Integer.parseInt(b);
@@ -48,6 +70,11 @@ public class SuperDodawanie {
             Float ans = x + y;
             return ans.toString();
         }
+
+        float x = isFrac(a) ? parseFrac(a) : 0;
+        float y = isFrac(b) ? parseFrac(b) : 0;
+        if (x != 0 || y != 0) return String.valueOf(x + y);
+
         return a + b;
     }
 }
